8f779ab52d526a1ca6162a3a39e662487fc3a9ca
Miroslav Tushev <mtushe1@lsu.edu>
2017-12-01 19:43:44 -0600

    Add gitignore

--- settings.py
+++
@@ -1,5 +0,0 @@
-HOST = "irc.twitch.tv"
-PORT = 6667
-OAUTH = "" # generate from http://www.twitchapps.com/tmi/
-IDENT = "" # twitch account for which you used the oauth
-CHANNEL = ""  # twitch channel you are connecting to
